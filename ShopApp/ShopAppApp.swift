e4a4efafc85776b8c34a7dd86a2718a6f9a71442
Isudoxan <isudoxan.work@gmail.com>
2025-10-09 20:20:45 +0200

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Danylo Liubyi on 09.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
