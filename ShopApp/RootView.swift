42585b180a55f480ef3c05fb7a5df2913af38d31
Isudoxan <isudoxan.work@gmail.com>
2025-10-09 23:05:19 +0200

    Create basic project.

---
+++ ShopApp/RootView.swift
@@ -0,0 +1,17 @@
+//
+//  RootView.swift
+//  ShopApp
+//
+//  Created by Danylo Liubyi on 09.10.2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Page")
+            .font(.headline)
+            .multilineTextAlignment(.center)
+            .padding()
+    }
+}
--- ShopApp/ShopAppApp.swift
+++ ShopApp/ShopAppApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct ShopAppApp: App {
+struct ShopApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
