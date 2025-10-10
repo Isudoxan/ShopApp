760e2fd95ec0f70d650c7f51e7ffd703df6388c1
Isudoxan <isudoxan.work@gmail.com>
2025-10-10 02:00:17 +0200

    Create Product detail view.

---
+++ ShopApp/Views/ProductDetailView.swift
@@ -0,0 +1,17 @@
+//
+//  ProductDetailView.swift
+//  ShopApp
+//
+//  Created by Danylo Liubyi on 10.10.2025.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    
+    var product: Product
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
